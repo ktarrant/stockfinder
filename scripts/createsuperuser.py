cc0af3239f0c2ce27d473cd82aecbabff096c6a0
ktarrant <kmtarrant13@gmail.com>
2016-05-28 23:48:05 -0400

    add try around createsuperuser

--- scripts/createsuperuser.py
+++ scripts/createsuperuser.py
@@ -1,5 +1,9 @@
 import django
 import os
+import logging
+import django.db.utils
+
+log = logging.getLogger(__info__)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crabotter.settings.prod")
@@ -8,7 +12,10 @@ if __name__ == "__main__":
 
     from django.contrib.auth.models import User
 
-    User.objects.create_superuser(
-        username=os.environ["SUPERUSER_USERNAME"],
-        email=os.environ["SUPERUSER_EMAIL"],
-        password=os.environ["SUPERUSER_PASSWORD"])
+    try:
+        User.objects.create_superuser(
+            username=os.environ["SUPERUSER_USERNAME"],
+            email=os.environ["SUPERUSER_EMAIL"],
+            password=os.environ["SUPERUSER_PASSWORD"])
+    except django.db.utils.IntegrityError:
+        log.info("superuser already exists. Skipping createsuperuser.")
