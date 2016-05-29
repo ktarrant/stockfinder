69082210f5eb27f9de5a63a71307b0467b55980b
ktarrant <kmtarrant13@gmail.com>
2016-05-28 23:52:56 -0400

    fix superuser

--- scripts/createsuperuser.py
+++ scripts/createsuperuser.py
@@ -3,7 +3,7 @@ import os
 import logging
 import django.db.utils
 
-log = logging.getLogger(__info__)
+log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crabotter.settings.prod")
